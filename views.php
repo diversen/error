8e0b35cf153fc908cf19ea6e1e0151dabe7c0055
Dennis Iversen <dennis.iversen@gmail.com>
2014-12-19 18:52:20 +0100

    refactor

--- views.php
+++ views.php
@@ -1,5 +1,7 @@
 <?php
 
+use diversen\moduleloader;
+
 class error_views {
    /**
     * controller file for error/display/404
@@ -10,8 +12,8 @@ class error_views {
         header("HTTP/1.1 403 Forbidden");
         template::setTitle(lang::translate('Error 403: Access Denied'));
         echo '<p>' . lang::translate('Error 403: Access Denied') . '</p>';
-        if (isset(error_module::$message)) {
-            echo '<p>' . error_module::$message . '</p>';
+        if (isset(moduleloader::$message)) {
+            echo '<p>' . moduleloader::$message . '</p>';
         }
     }
     
@@ -26,6 +28,4 @@ class error_views {
         template::setTitle(lang::translate('Error 404: Page was not found'));
         echo '<p>' . lang::translate('Error 404: Page was not found') . '</p>';
     }
-    
-   
 }
