1fef2ed926d77a9cb1dd3bca61a8b19a5c51a54a
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:36:15 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,8 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/error/views.php
+
+$LANG['Error 403: Access Denied'] = 'Error 403: Access Denied';
+$LANG['Error 404: Page was not found'] = 'Error 404: Page was not found';
